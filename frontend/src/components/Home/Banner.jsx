56ec3a246b9f3ee513d13beb2a5835baf7db5dc1
Devendra <devendra94kumawat@gmail.com>
2025-11-15 17:04:51 +0530

    Create frontend app using vite, Complete Home UI.

---
+++ frontend/src/components/Home/Banner.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Banner = () => {
+  return (
+    <div className="w-full py-2.5 font-medium text-sm text-[#1A73E8] text-center bg-gradient-to-r from-[#1A73E8] to-[#E8F1FF]">
+      <p>
+        <span className="px-3 py-1 rounded-lg text-white bg-[#1A73E8] mr-2">
+          New
+        </span>
+        AI Feature Added
+      </p>
+    </div>
+  );
+};
+
+export default Banner;
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Banner from "../components/Home/Banner";
+import Hero from "../components/Home/Hero";
+import Features from "../components/Home/Features";
+
+const Home = () => {
+  return (
+    <div>
+      <Banner />
+      <Hero />
+      <Features />
+    </div>
+  );
+};
+
+export default Home;
