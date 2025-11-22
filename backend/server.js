6c803b672c6f6f3d8f88bb235484118d597ddd52
Devendra <devendra94kumawat@gmail.com>
2025-11-22 15:23:26 +0530

    Write resume controllers, routes , multer middleware, integrate imageKit.

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import "dotenv/config";
 import connectDB from "./config/db.js";
 import userRouter from "./routes/user.routes.js";
+import resumeRouter from "./routes/resume.routes.js";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -17,6 +18,7 @@ app.get("/", (req, res) => {
   res.send("Server is live");
 });
 app.use("/api/users", userRouter);
+app.use("/api/resumes", resumeRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
